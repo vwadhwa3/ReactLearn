8f27136ae6463bd0ecc69a7423b1e216a96ba6ac
Varun Wadhwa <vwadhwa3@gmail.com>
2023-12-25 15:32:20 +0530

    going out

---
+++ App.js
@@ -0,0 +1,20 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+const parent = React.createElement("div", { id: "parent" }, [
+    React.createElement("div", { id: "child" }, [
+      React.createElement("h1", {}, "This is Namaste React 🚀"),
+      React.createElement("h2", {}, "by Varun"),
+    ]),
+    React.createElement("div", { id: "child2" }, [
+      React.createElement("h1", {}, "I'm an h1 tag"),
+      React.createElement("h2", {}, "I'm an h2 tag"),
+    ]),
+  ]);
+
+
+console.log(parent); // object
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(parent);
