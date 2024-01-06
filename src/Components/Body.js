2895e18390d03487c0729afac22d889684a8181d
Varun Wadhwa <vwadhwa3@gmail.com>
2024-01-06 20:36:36 +0530

    body

--- src/Components/Body.js
+++ src/Components/Body.js
@@ -1,10 +1,25 @@
 import RestaurantCard  from "./RestaurantCard"
 import  {restList} from "../utils/mockData"
-import { useState } from "react"
+import { useEffect, useState } from "react"
 
 
 const Body= ()=>{
-     const [listOfRestaurants,setlistOfRestaurants] =useState(restList);
+     const [listOfRestaurants,setlistOfRestaurants] =useState(null);
+     useEffect (()=>{
+        console.log("useEffect called")
+        fetchData()
+     },[])
+     console.log("body rendered")
+
+     const fetchData = async ()=>{
+     const data = await fetch("https://www.swiggy.com/dapi/restaurants/list/v5?lat=28.5273336&lng=77.219241&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTI")
+     const json =await data.json();
+     console.log("card 4")
+     console.log(json.data.cards[4].card.card.gridElements.infoWithStyle.restaurants[0].info)
+     console.log("fetchData function")
+     setlistOfRestaurants(await json.data.cards[4].card.card.gridElements.infoWithStyle.restaurants)
+    }
+
     return (
                 <div className="body" >
                         <div className="filter">
@@ -21,7 +36,7 @@ const Body= ()=>{
                         </div>
                         <div className="res-conatiner"> 
                         {
-                            listOfRestaurants.map(x => <RestaurantCard  key={x.info.id} resData={x} /> )
+                            listOfRestaurants?.map(x => <RestaurantCard  key={x.info.id} resData={x} /> )
                         }
                         </div>
                 </div>
