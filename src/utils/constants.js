3e36bafb63fd94abc6dd6079b53998f7ab82586d
Varun Wadhwa <vwadhwa3@gmail.com>
2024-01-11 14:28:29 +0530

    RestaurantMenu_API

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -4,3 +4,6 @@ export const LOGO_URL ="https://www.logodesign.net/logo/smoking-burger-with-lett
 
 export const MENU_API =
   "https://www.swiggy.com/dapi/restaurants/list/v5?lat=28.62993&lng=77.140996&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING";
+  
+
+const RestaurantMenu_API= "https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=28.62993&lng=77.140996&restaurantId=104689&submitAction=ENTER"
