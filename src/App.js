ccbcf0eef2ed420adbf50578e11809af5fcb6e36
Varun Wadhwa <vwadhwa3@gmail.com>
2024-01-18 22:30:36 +0530

    about lazy

--- src/App.js
+++ src/App.js
@@ -3,14 +3,14 @@ import ReactDOM from "react-dom/client";
 import Header from "./Components/Header";
 import  Body  from "./Components/Body";
 import { createBrowserRouter ,RouterProvider,Outlet} from "react-router-dom";
-import About from "./Components/About";
+//import About from "./Components/About";
 import Contact from "./Components/Contact";
 import Error from "./Components/Error";
 import RestaurantMenu from "./Components/RestaurantMenu";
  import Shimmer from "./Components/Shimmer";
  
 const Grocery  =  lazy(()=> import("./Components/Grocery"))
-
+const About = lasy (()=>  import("./Components/About"))
 const Footer = () => {
     return (
         <div className="">
@@ -39,7 +39,7 @@ const appRouter = createBrowserRouter([
             },
             {
                 path:"/about",
-                element:<About/>
+                element:<Suspense fallback= {<Shimmer/>}> <About/></Suspense> 
                 
             },
             {
