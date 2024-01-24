df2eb22dac82c41231746cb15a731db7721250ea
Varun Wadhwa <vwadhwa3@gmail.com>
2024-01-24 14:25:24 +0530

    resCategoriesList

--- src/Components/ResCategories.js
+++ src/Components/ResCategories.js
@@ -1,9 +1,22 @@
+import { title } from "process"
+import ResCategoriesList from "./ResCategoriesList"
 const  ResCategories = (props) =>{
     console.log(props)
+  const  list  = props.data.itemCards
+  //console.log(list)
+    const {title } = props?.data
+
     return(
         <div>
-            <div>hello </div>
+            <div className=" w-6/12 mx-auto my-4 bg-gray-100 shodow-lg p-4 ">
+                <div className="flex justify-between">
+                    <span className="font-bold text-lg">{title} ({props?.data.itemCards.length})</span> 
+                    <span>🔽</span> 
+                </div>  
+                <ResCategoriesList items ={props.data.itemCards} />          
+            </div>
         </div>
     )
 }
 export default ResCategories
+ 
