4ed43ca56f4d7ebcb5706df6058911aa99345280
Varun Wadhwa <vwadhwa3@gmail.com>
2024-01-24 22:09:00 +0530

    open and close

--- src/Components/ResCategories.js
+++ src/Components/ResCategories.js
@@ -1,19 +1,24 @@
-import { title } from "process"
+import { constrainedMemory, title } from "process"
 import ResCategoriesList from "./ResCategoriesList"
+import { useState } from "react"
 const  ResCategories = (props) =>{
     console.log(props)
   const  list  = props.data.itemCards
-  //console.log(list)
+  const [showItems , setShowItems]= useState(false)
     const {title } = props?.data
+    const handleClick =() =>{
+        setShowItems(!showItems)
+    }
 
+    
     return(
         <div>
             <div className=" w-6/12 mx-auto my-4 bg-gray-100 shodow-lg p-4 ">
-                <div className="flex justify-between">
+                <div className="flex justify-between  cursor-pointer" onClick={handleClick}>
                     <span className="font-bold text-lg">{title} ({props?.data.itemCards.length})</span> 
                     <span>🔽</span> 
                 </div>  
-                <ResCategoriesList items ={props.data.itemCards} />          
+                 {showItems  &&  <ResCategoriesList items ={props.data.itemCards} /> }
             </div>
         </div>
     )
