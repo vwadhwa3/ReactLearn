6a694b68339e1f4369ee6234c251a642f953f92e
varun wadhwa <vwadhwa3@gmail.com>
2024-01-31 22:28:24 +0530

    cart

--- src/Components/Cart.js
+++ src/Components/Cart.js
@@ -1,12 +1,19 @@
-import { useSelector } from "react-redux"
+import { useSelector,useDispatch } from "react-redux"
 import ResCategoriesList from "./ResCategoriesList";
+import {clearCart} from '../utils/cartSlice'
 const Cart =() =>{
     const cartItems = useSelector((store)=> store.cart.items);
+    const dispatch = useDispatch()
+    const handleCartItems = () =>{
+         dispatch(clearCart())
+    }
     return(
         <div className="text-center m-4 p-4">
              <h1 className="font-bold">Cart</h1>
              <div className="w-6/12 m-auto">
-                <ResCategoriesList items={cartItems}/>
+                <button className="p-2 m-2 bg-black text-white rounded-lg" onClick={clearCart}> clear Cart</button>
+                {cartItems.length == 0  && (<h1>addItem</h1>)}
+                <ResCategoriesList items={handleCartItems}/>
             </div>
         </div>
     )
--- src/Components/ResCategoriesList.js
+++ src/Components/ResCategoriesList.js
@@ -9,7 +9,7 @@ const ResCategoriesList = ({items , dummyData}) => {
     }
     return (
       <div>
-        {items.map((item) => (
+        {items?.map((item) => (
           <div
             key={item.card.info.id}
             className="p-2 m-2 border-gray-200 border-b-2 text-left flex justify-between"
